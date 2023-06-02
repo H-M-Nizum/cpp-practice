4467b29bbb581bbc84ff5757f3fbf6ca31b52a4a
H-M-Nizum <hmnizum1714032@gmail.com>
2023-06-02 15:42:28 +0600

    ms

---
+++ 2_min_max_swap_built_in_function.cpp
@@ -0,0 +1,23 @@
+// max or min into two number in c++.
+
+// need #include<algorithm> header file.
+// int a, b;
+// min(a, b);
+// max(a, b);
+
+// if we need min or max from 3 or more value.
+// min({a, b, c, ....})
+// max({a, b, c, ....})
+
+// if find min or max in a ARRAY.
+// int n;
+// cin>>n;
+// int a[n];
+// *max_element(a, a+n);
+// *min_element(a, a+n);
+
+
+
+// #include<utility> for swape two value.
+// int a = 10, b = 20;
+// swap(a, b); -->> a=20, b=10
